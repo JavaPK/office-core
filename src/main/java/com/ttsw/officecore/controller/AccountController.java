07fd18499e3a2e9c2985ab1490439ccc4f35826c
juliach1 <119865435+juliach1@users.noreply.github.com>
2023-11-30 18:04:52 +0100

    Create account activation

--- src/main/java/com/ttsw/officecore/controller/AccountController.java
+++ src/main/java/com/ttsw/officecore/controller/AccountController.java
@@ -1,14 +1,27 @@
 package com.ttsw.officecore.controller;
 
+import com.ttsw.officecore.api.ApiConstants;
 import com.ttsw.officecore.service.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
 public class AccountController {
 
     private final AccountService accountService;
+
+    @PostMapping(ApiConstants.API_OFFICE_CORE + "/account/activate/{token}")
+    private ResponseEntity activateAccount(@PathVariable UUID token) {
+        accountService.activateAccount(token);
+
+        return ResponseEntity.ok(null);
+    }
 
 
 }
--- src/main/java/com/ttsw/officecore/service/AccountService.java
+++ src/main/java/com/ttsw/officecore/service/AccountService.java
@@ -1,5 +1,9 @@
 package com.ttsw.officecore.service;
 
+import java.util.UUID;
 
 public interface AccountService {
+
+    void activateAccount(UUID token);
+
 }
