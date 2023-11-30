24b0e07e515d5ad16b26008c88f0cce94d22e295
juliach1 <119865435+juliach1@users.noreply.github.com>
2023-11-30 18:04:52 +0100

    Create account activation

---
+++ src/main/java/com/ttsw/officecore/api/ApiConstants.java
@@ -0,0 +1,6 @@
+package com.ttsw.officecore.api;
+
+public class ApiConstants {
+
+    public static final String API_OFFICE_CORE = "/api/office-core";
+}
--- src/main/java/com/ttsw/officecore/service/AccountServiceImpl.java
+++ src/main/java/com/ttsw/officecore/service/AccountServiceImpl.java
@@ -3,7 +3,15 @@ package com.ttsw.officecore.service;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @NoArgsConstructor
 @Service
 public class AccountServiceImpl implements AccountService {
+
+    @Override
+    public void activateAccount(UUID token) {
+
+    }
+
 }
