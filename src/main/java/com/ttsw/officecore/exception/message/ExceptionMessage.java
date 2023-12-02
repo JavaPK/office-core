8fee4792f7929219f98322b9c740f99ec0887ec4
Hubert Kulka <hubert.kulka01@gmail.com>
2023-12-02 22:54:23 +0100

    account creation exceptions update

--- src/main/java/com/ttsw/officecore/exception/message/ExceptionMessage.java
+++ src/main/java/com/ttsw/officecore/exception/message/ExceptionMessage.java
@@ -7,7 +7,10 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ExceptionMessage {
 
-    EMAIL_ALREADY_EXISTS("20231130:173100", "Email already exists.");
+    EMAIL_ALREADY_EXISTS("20231130:173100", "Email already exists."),
+    EMAIL_NOT_VALID("20231202:224300:", "Email is not valid."),
+    PASSWORD_NOT_VALID("20231202:224500", "Password is not valid."),
+    NULL_EMAIL_OR_PASSWORD("20231202:223600", "Email or password is null.");
 
     private String code;
     private String message;
